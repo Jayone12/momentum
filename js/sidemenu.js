1e37d85d85e16b25b0f46d76f4931d61604de5d4
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-04-25 04:43:55 +0900

    fix: fixed side menu

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,6 +1,8 @@
 const sideMenu = document.querySelector(".main-menu");
 const menuBtn = document.querySelector(".menu-btn");
 const closeBtn = document.querySelector(".close-btn");
+const menuTitle = document.querySelector(".menu-title");
+const toDoListContiner = document.querySelector(".todo-list-container");
 
 function openMenu() {
   sideMenu.classList.add("active");
@@ -10,5 +12,10 @@ function closeMenu() {
   sideMenu.classList.remove("active");
 }
 
+function openMenuContent() {
+  toDoListContiner.classList.toggle("active");
+}
+
 menuBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", closeMenu);
+menuTitle.addEventListener("click", openMenuContent);
