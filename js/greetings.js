b26def14217116c62a3e734fa1902fbd14d1a8a1
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-04-27 20:33:01 +0900

    fix: bug fix

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,5 @@
 const loginForm = document.querySelector(".login-modal");
 const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
 const menuUserName = document.querySelector(".user-name");
 
 const HIDDEN_CLASSNAME = "hidden";
@@ -17,14 +16,14 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   const greetingArr = ["Good morning!", "Good afternoon!", "Good evening!"];
   const getHour = new Date().getHours();
-  if (0 < getHour > 12) {
-    greeting.innerText = `${greetingArr[0]} ${username}`;
-  } else if (12 < getHour > 17) {
-    greeting.innerText = `${greetingArr[1]}  ${username}`;
-  } else if (getHour > 17) {
-    greeting.innerText = `${greetingArr[2]}  ${username}`;
+  console.log(getHour < 12);
+  if (getHour < 12) {
+    menuUserName.innerText = `${greetingArr[0]} ${username}`;
+  } else if (getHour < 17) {
+    menuUserName.innerText = `${greetingArr[1]} ${username}`;
+  } else if (getHour < 24) {
+    menuUserName.innerText = `${greetingArr[2]} ${username}`;
   }
-  menuUserName.innerText = `Welcome to ${username}!`;
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
