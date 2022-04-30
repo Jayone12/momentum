ceeff6263d1b7e02ae7d143f4b924270e51930ef
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-04-30 21:48:20 +0900

    fix: Delete unnecessary code

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,6 @@ const API_KEY = "27773098d92b3037352b09fea1033540";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log(lat, lng);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&lang=kr&units=metric`;
   fetch(url)
     .then((res) => res.json())
