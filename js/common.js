5a4b824b34876d78086428cbc8b38719bdb43bc4
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-04-30 23:06:23 +0900

    fix: Add alram function

--- js/common.js
+++ js/common.js
@@ -1,9 +1,12 @@
 import { backgroundRender } from "./background.js";
 import { getClock } from "./clock.js";
+import { alramClock } from "./alram.js";
 
 function init() {
   getClock();
   backgroundRender();
+  alramClock();
 }
 
+init();
 setInterval(init, 1000);
