675c60f5e1441000489cec996a486345f21750a9
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-05-02 17:11:35 +0900

    refator: Split the current date into a function

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,17 @@
 const clock = document.querySelector("h2#clock");
 
-export function getClock() {
+function nowDate() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, 0);
+  const hour = String(date.getHours()).padStart(2, 0);
   const minutes = String(date.getMinutes()).padStart(2, 0);
   const seconds = String(date.getSeconds()).padStart(2, 0);
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+  return { hour, minutes, seconds };
 }
+
+function getClock() {
+  const { hour, minutes, seconds } = nowDate();
+  clock.innerText = `${hour}:${minutes}:${seconds}`;
+}
+
+export { nowDate, getClock };
