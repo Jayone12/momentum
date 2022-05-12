e39245dfa8d23111493d024816bbfc07ef4644b3
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-05-12 14:37:58 +0900

    feat: add widget function

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,15 @@
 import { saveLocalStorage, getLocalStorage } from "./localStorage.js";
+import { widgetRander } from "./widget.js";
 
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+const widetLists = document.querySelector(".todo-widget > ul");
+const todoWidget = document.querySelector(".todo-widget");
+const widgetBtn = document.querySelector(".menu-todo > .widget-btn");
 
 const TODOS_KEY = "todos";
+const WIDGET_KEY = "widget";
 let toDos = [];
 
 function deleteToDo(event) {
@@ -14,37 +19,63 @@ function deleteToDo(event) {
   saveLocalStorage(TODOS_KEY, toDos);
 }
 
-function checked(event) {
-  const li = event.target.parentElement;
-  const check = document.querySelector(`input[name="${event.target.name}"]`);
-
-  if (check.checked) {
-    li.style.textDecoration = "line-through";
-    li.style.color = "#c8c8c8";
+function handleCheck(event, newToDo) {
+  const todoText = event.path[1].lastChild;
+  newToDo.check = !newToDo.check;
+  if (!newToDo.check) {
+    todoText.style.textDecoration = "none";
+    todoText.style.color = "#000";
   } else {
-    li.style.textDecoration = "none";
-    li.style.color = "#000";
+    todoText.style.textDecoration = "line-through";
+    todoText.style.color = "#c8c8c8";
   }
+  saveLocalStorage(TODOS_KEY, toDos);
+  widgetRander(TODOS_KEY, widetLists);
 }
 
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   li.id = newToDo.id;
   const div = document.createElement("div");
-  const p = document.createElement("p");
-  const input = document.createElement("input");
-  input.name = newToDo.id;
-  input.type = "checkbox";
-  input.addEventListener("click", checked);
-  p.innerText = newToDo.text;
+  li.appendChild(div);
+  checkInputCreate(div, newToDo);
+  pragCreate(div, newToDo);
+  deleteBtnCreate(li);
+  toDoList.appendChild(li);
+}
+
+function deleteBtnCreate(parentEl) {
   const button = document.createElement("button");
   button.classList.add("fas", "fa-trash-alt");
-  button.addEventListener("click", deleteToDo);
-  li.appendChild(div);
-  div.prepend(input);
-  div.appendChild(p);
-  li.appendChild(button);
-  toDoList.appendChild(li);
+  button.addEventListener("click", (event) => {
+    deleteToDo(event);
+    widgetRander(TODOS_KEY, widetLists);
+  });
+  parentEl.appendChild(button);
+}
+
+function pragCreate(parentEl, value) {
+  const p = document.createElement("p");
+  if (!value.check) {
+    p.style.textDecoration = "none";
+    p.style.color = "#000";
+  } else {
+    p.style.textDecoration = "line-through";
+    p.style.color = "#c8c8c8";
+  }
+  p.innerText = value.text;
+  parentEl.appendChild(p);
+}
+
+function checkInputCreate(parentEl, value) {
+  const input = document.createElement("input");
+  input.name = value.id;
+  input.type = "checkbox";
+  input.checked = value.check;
+  input.addEventListener("click", (event) => {
+    handleCheck(event, value);
+  });
+  parentEl.prepend(input);
 }
 
 function handleToDoSubmit(event) {
@@ -54,17 +85,37 @@ function handleToDoSubmit(event) {
   const newToDoObj = {
     text: newToDo,
     id: Date.now(),
+    check: false,
   };
   toDos.push(newToDoObj);
   paintToDo(newToDoObj);
   saveLocalStorage(TODOS_KEY, toDos);
+  widgetRander(TODOS_KEY, widetLists);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+const getToDos = JSON.parse(getLocalStorage(TODOS_KEY));
 
-const savedToDos = getLocalStorage(TODOS_KEY);
-if (savedToDos !== null) {
-  const parsedToDos = JSON.parse(savedToDos);
-  toDos = parsedToDos;
-  parsedToDos.forEach(paintToDo);
+if (getToDos !== null) {
+  toDos = getToDos;
+  toDos.forEach(paintToDo);
+  widgetRander(TODOS_KEY, widetLists);
+}
+
+const getWidget = JSON.parse(getLocalStorage(WIDGET_KEY));
+
+widgetBtn.addEventListener("click", () => {
+  getWidget[0].check = !getWidget[0].check;
+  if (getWidget[0].check) {
+    todoWidget.classList.remove("hidden");
+  } else {
+    todoWidget.classList.add("hidden");
+  }
+  saveLocalStorage(WIDGET_KEY, getWidget);
+});
+
+if (getWidget[0].check) {
+  todoWidget.classList.remove("hidden");
+} else {
+  todoWidget.classList.add("hidden");
 }
